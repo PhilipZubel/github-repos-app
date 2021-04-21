9b9e1febbb792925377f52ed30a20c30b647494c
PhilipZubel <49106470+PhilipZubel@users.noreply.github.com>
2021-04-22 00:18:01 +0100

    main functionality completed

--- github_users/views.py
+++ github_users/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 import requests
 
-# Create your views here.
-
 def index(request):
     # values passed to the template will be stored in context dictionary
     context = {}
  
     # check if a GET request is made
     if 'username' in request.GET:
+        context['GET_request_exists'] = True
         username = request.GET['username']
+        context['username'] = username
         # url accessed to get a response
         url = 'https://api.github.com/users/%s' % username
         # get the response from the api
@@ -28,30 +28,42 @@ def index(request):
 
         context['response'] = response.json()
         
+        # get the values for each repository if the api call is successful
         if is_search_successful:
+            # get a response from the url which contains data about repositories
             url_repos = context['response']['repos_url']
             response_repos = requests.get(url_repos)
 
+            # get total number of stars
             context['stars_count'] = getTotalStargazerCount(response_repos.json())
+            # get repository names, stars, and urls
             context['repos'] = getGitHubRepositories(response_repos.json())
 
+            # update the remaining responses count since a new response was created
+            context['rate']['remaining'] = response_repos.headers['X-RateLimit-Remaining']
+    else:
+        context['GET_request_exists'] = False
+    
     return render(request, 'github_users/index.html', context)
 
+# returns the total number of stars of all repositories
 def getTotalStargazerCount(response):
-    stars_count = 0
+    stars_sum = 0
+    # loop through all repositories and add the stars count for each repo to the sum
     for repo in response:
-        stars_count += repo['stargazers_count']
-    return stars_count
+        stars_sum += repo['stargazers_count']
+    return stars_sum
 
+# returns the name, stargazer count, and url link for all user's repositories
 def getGitHubRepositories(response):
     repos_data = {}
     for repo in response:
-        # Since repository names are unique for a user, those names can be used as keys to the dictionary
+        # since repository names are unique for a single user, 
+        # those names can be used as keys to the dictionary
         repos_data[repo['name']] = {
             'stars' : repo['stargazers_count'],
             'url' : repo['html_url'],
         }
-
     return repos_data
 
 
